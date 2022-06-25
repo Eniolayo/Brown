3eba8f3ad4691f66f42c410dbc1f1dbdc25da8e6
Eniolayo <eniolayodeji@gmail.com>
2022-06-25 12:06:37 +0100

    recent changes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
     <>
-    <footer class="footer">
+      <footer class="footer">
         <ul class="footer__list">
-            <li class="footer__link"><a href="">linkedIn</a></li>
-            <li class="footer__link"><a href="https://twitter.com/AlukoBrown" target="blank">twitter</a></li>
-            <li class="footer__link"><a href="https://www.instagram.com/alukobrown/" target="blank">instagram</a></li>
-            <li class="footer__link"><a href="https://dribbble.com/alukobrown" target="blank">medium</a></li>
+          <li class="footer__link">
+            <a href="">linkedIn</a>
+          </li>
+          <li class="footer__link">
+            <a href="https://twitter.com/AlukoBrown" target="blank">
+              twitter
+            </a>
+          </li>
+          <li class="footer__link">
+            <a href="https://www.instagram.com/alukobrown/" target="blank">
+              instagram
+            </a>
+          </li>
+          <li class="footer__link">
+            <a href="https://dribbble.com/alukobrown" target="blank">
+              medium
+            </a>
+          </li>
         </ul>
-    </footer>
+      </footer>
     </>
-  )
+  );
 }
