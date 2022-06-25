b1eef77e766d18ae2a3d55eee03bc538bf819292
oladips <oladeleoladipupo555@gmail.com>
2022-06-25 11:06:22 +0100

    first attempt

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+import Cursor from './components/cursor';
+import Header from './components/header';
+import Main from './components/main';
+import Footer from './components/footer';
 
 function App() {
   return (
     <>
+    <div>
+      <Cursor />
+      <Header />
+      <Main />
+      <Footer />
+      
+
+
+
+    </div>
     </>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+    <footer class="footer">
+        <ul class="footer__list">
+            <li class="footer__link"><a href="">linkedIn</a></li>
+            <li class="footer__link"><a href="https://twitter.com/AlukoBrown" target="blank">twitter</a></li>
+            <li class="footer__link"><a href="https://www.instagram.com/alukobrown/" target="blank">instagram</a></li>
+            <li class="footer__link"><a href="https://dribbble.com/alukobrown" target="blank">medium</a></li>
+        </ul>
+    </footer>
+    </>
+  )
+}
